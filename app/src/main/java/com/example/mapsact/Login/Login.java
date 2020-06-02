571cf6a7c68916c2b52aa1bb0cf3c398899a83a3
henriquesaenger <henriquesaenger@gmail.com>
2020-06-02 08:56:41 -0300

    Commit

--- app/src/main/java/com/example/mapsact/Login/Login.java
+++ app/src/main/java/com/example/mapsact/Login/Login.java
@@ -10,14 +10,23 @@ import android.widget.Button;
 import com.example.mapsact.MapsActivity;
 import com.example.mapsact.R;
 
+import io.realm.Realm;
+import io.realm.RealmObject;
+
 public class Login extends AppCompatActivity {
 
     private Button btnLogin;
     private Button btnSignUp;
+    Realm inst= Realm.getDefaultInstance();            //cria uma instancia do realm
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        Realm.init(this);           //inicializa o Realm
+
+
+
+
 
         btnLogin = (Button) findViewById(R.id.Login);
         btnSignUp = (Button) findViewById(R.id.Signup);
@@ -40,6 +49,17 @@ public class Login extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    //insert database
+    public void insert(String usuario, String senha){
+        inst.beginTransaction();
+
+        RealmLogin user= inst.createObject(RealmLogin.class);
+        user.setUser(usuario);
+        user.setPass(senha);
+
+        inst.commitTransaction();
+
     }
 
 
